fd82f334949d1914a1fc515d6ce582b3fd92fc64
Ayush Anand <anandayush865@gmail.com>
2024-10-13 00:28:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter ,  Routes, Route ,  } from 'react-router-dom';
 import './App.css';
 import './Responsive.css'
 import About from './Pages/About';
-import Footer from './Component/Footer';
+
 
 function App() {
   return (
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -11,27 +11,27 @@ export default function Navbar() {
 
             <div class="navbar">
                 <span className="logoAndserach">
-                    <img id="sae" src={saelogo} />
+                    <img id="sae" src={saelogo} alt='logo' />
                     <span id="search"><FaSearch /> <input type="text" placeholder="Search Opportunities" /></span>
                 </span>
                 <ul className='servicelist'>
-                    <li><a id="learn" href="">Learn</a></li>
-                    <li><a id="practice" href="">Practice</a></li>
-                    <li><a id="mentor" href="">Mentorship</a></li>
-                    <li><a id="compete" href="/">Compete</a> </li>
-                    <li><a id="intern" href="">Internships</a></li>
-                    <li><a id="jobs" href="">Jobs</a></li>
-                    <li><a id="login" href="">Login</a></li>
-                    <li><a id="host" href="">< IoMdAdd />Host</a></li>
-                    <li><a id="host" href=""><MdOutlineShoppingBag />For Business</a></li>
+                    <li><a id="learn"  href='/'>Learn</a></li>
+                    <li><a id="practice" href='/'>Practice</a></li>
+                    <li><a id="mentor" href='/'>Mentorship</a></li>
+                    <li><a id="compete" href='/'>Compete</a> </li>
+                    <li><a id="intern" href='/'>Internships</a></li>
+                    <li><a id="jobs" href='/'>Jobs</a></li>
+                    <li><a id="login" href='/'>Login</a></li>
+                    <li><a id="host" href='/'>< IoMdAdd />Host</a></li>
+                    <li><a id="host" href='/'><MdOutlineShoppingBag />For Business</a></li>
                 </ul>
             </div>
         {/* mobile nav */}
             <div className="moblienavbar">
-                <span><img id="sae" src={saelogo} /></span>
+                <span><img id="sae" src={saelogo} alt='logo'/></span>
                  <ul>
                     <li><FaSearch /></li>
-                    <li><li><a id="login" href="">Login</a></li></li>
+                    <li><li><a id="login" href='/'>Login</a></li></li>
                     <li><MdOutlineShoppingBag /></li>
                  </ul>
             </div>
